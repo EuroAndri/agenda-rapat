149582f7c4c75f0dda649257a437b1f62e2d7d82
EuroAndri <127416226+EuroAndri@users.noreply.github.com>
2025-09-18 13:53:04 +0700

    Mencoba Pull di Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Menjalankan seeder PenggunaSeeder dan RapatSeeder
+        //Memanggil seeder lainnya
         $this->call([
             PenggunaSeeder::class,  
             RapatSeeder::class,     
