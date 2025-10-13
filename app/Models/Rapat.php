56c6c5ac855f4dca2b23bc71bb4d8d5d12295573
EuroAndri <127416226+EuroAndri@users.noreply.github.com>
2025-10-13 12:53:26 +0700

    Revisi HasUuid

--- app/Models/Rapat.php
+++ app/Models/Rapat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Support\Str;
 use App\Models\Pengguna;
 use App\Models\Kehadiran;
